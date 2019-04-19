92158a74f966e274d23212d2da3ee9a731953149
sagar patil <sagar57patil57@gmail.com>
2019-04-19 20:09:28 +0530

    make it better

---
+++ public/sw.js
@@ -0,0 +1,68 @@
+var CACHE_STATIC_NAME = 'static-v120';
+var CACHE_DYNAMIC_NAME = 'dynamic-v120';
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/info.html',
+          '/about.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/images/ch.jpg',
+          '/src/images/chazy.jpg',
+          '/src/images/whatches.jpg',
+          '/src/images/sf-boat.jpg',
+          'https://code.jquery.com/jquery-3.1.1.min.js',
+          'https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css',
+          'https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js'
+        ]);
+      })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function(err) {
+
+            });
+        }
+      })
+  );
+});
